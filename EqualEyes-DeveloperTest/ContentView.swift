6635d07fb736419ca1ba06a23993988f71c0a780
Boris Herman <boris42@mac.com>
2020-06-23 21:55:14 +0200

    Initial Commit

---
+++ EqualEyes-DeveloperTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EqualEyes-DeveloperTest
+//
+//  Created by Boris Herman on 23/06/2020.
+//  Copyright © 2020 Sight&Sound s.p. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
